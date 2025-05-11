3acc3148fec740a54e8cafb0de38b7692993f24e
Umabharathi764 <66896938+Umabharathi764@users.noreply.github.com>
2025-05-11 23:15:42 +0530

    Add files via upload

--- LearnCucumber/src/main/java/runner/RunnerClass.java
+++ LearnCucumber/src/main/java/runner/RunnerClass.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features ="src/main/java/features/CreateAccountName.feature" ,glue="steps" ,monochrome=true ,publish=true)
+@CucumberOptions(features ="src/main/java/features" ,glue={"steps","hooks"} ,monochrome=true ,publish=true,tags="@Lead")
 public class RunnerClass extends AbstractTestNGCucumberTests {
 
 }
